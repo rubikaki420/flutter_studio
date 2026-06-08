65e6849cb2eacaa34fceffd0812fbac61a71c8b6
Anon4You <katilchora666@gmail.com>
2026-06-08 05:47:51 +0530

    revert targetSdk from 34 to 28 for exec() compat

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 28
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
